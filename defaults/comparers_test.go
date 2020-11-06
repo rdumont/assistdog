168b06230d14b836457a99d575117f86cab91408
Rodrigo Dumont <rlsdumont@gmail.com>
2020-11-06 11:00:18 +0100

    test: add tests for each comparer and parser

---
+++ defaults/comparers_test.go
@@ -0,0 +1,74 @@
+package defaults
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompareString(t *testing.T) {
+	t.Run("returns nil for equal strings", func(t *testing.T) {
+		err := CompareString("abc", "abc")
+
+		require.NoError(t, err)
+	})
+
+	t.Run("returns error for actual that isn't a string", func(t *testing.T) {
+		err := CompareString("abc", 123)
+
+		require.EqualError(t, err, "123 is not a string")
+	})
+
+	t.Run("returns error for different strings", func(t *testing.T) {
+		err := CompareString("abc", "ABC")
+
+		require.EqualError(t, err, "expected abc, but got ABC")
+	})
+}
+
+func TestCompareInt(t *testing.T) {
+	t.Run("returns nil for equal integers", func(t *testing.T) {
+		err := CompareInt("123", 123)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("returns error for actual that isn't an integer", func(t *testing.T) {
+		err := CompareInt("123", "not an integer")
+
+		require.EqualError(t, err, "not an integer is not an int")
+	})
+
+	t.Run("returns error for different integers", func(t *testing.T) {
+		err := CompareInt("123", 456)
+
+		require.EqualError(t, err, "expected 123, but got 456")
+	})
+}
+
+func TestCompareTime(t *testing.T) {
+	validTime, err := time.Parse(time.RFC3339, "2020-11-05T16:01:54Z")
+	require.NoError(t, err)
+
+	t.Run("returns nil for equal times", func(t *testing.T) {
+		err := CompareTime("2020-11-05T16:01:54Z", validTime)
+
+		require.NoError(t, err)
+	})
+
+	t.Run("returns error for actual that is not a time", func(t *testing.T) {
+		err := CompareTime("2020-11-05T16:01:54Z", "NOT A TIME")
+
+		require.EqualError(t, err, "NOT A TIME is not time.Time")
+	})
+
+	t.Run("returns error for different times", func(t *testing.T) {
+		differentTime := validTime.Add(1*time.Hour)
+		require.NoError(t, err)
+
+		err = CompareTime("2020-11-05T16:01:54Z", differentTime)
+
+		require.EqualError(t, err, "expected 2020-11-05 16:01:54 +0000 UTC, but got 2020-11-05 17:01:54 +0000 UTC")
+	})
+}
---
+++ defaults/parsers_test.go
@@ -0,0 +1,64 @@
+package defaults
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseString(t *testing.T) {
+	res, err := ParseString("abc")
+
+	assert.NoError(t, err)
+	assert.Equal(t, "abc", res)
+}
+
+func TestParseInt(t *testing.T) {
+	t.Run("parses valid integer", func(t *testing.T) {
+		res, err := ParseInt("123")
+
+		require.NoError(t, err)
+		require.Equal(t, 123, res)
+	})
+
+	t.Run("returns error for invalid integer", func(t *testing.T) {
+		_, err := ParseInt("abc")
+
+		require.EqualError(t, err, `strconv.Atoi: parsing "abc": invalid syntax`)
+	})
+}
+
+func TestParseTime(t *testing.T) {
+	t.Run("parses supported layouts", func(t *testing.T) {
+		expected, err := time.Parse(time.RFC3339Nano, "2020-11-05T16:01:54.0123Z")
+		require.NoError(t, err)
+
+		cases := []struct {
+			layout string
+			raw string
+			tolerance time.Duration
+		}{
+			{layout: time.RFC822, raw: "05 Nov 20 16:01 MST", tolerance: time.Minute},
+			{layout: time.RFC3339, raw: "2020-11-05T16:01:54Z", tolerance: time.Second},
+			{layout: time.RFC3339, raw: "2020-11-05T16:01:54+00:00", tolerance: time.Second},
+			{layout: time.RFC3339Nano, raw: "2020-11-05T16:01:54.0123Z", tolerance: time.Nanosecond},
+		}
+
+		for _, tc := range cases {
+			t.Run(tc.layout, func(t *testing.T) {
+				res, err := ParseTime(tc.raw)
+
+				require.NoError(t, err)
+				assert.WithinDuration(t, expected, res.(time.Time), tc.tolerance)
+			})
+		}
+	})
+
+	t.Run("returns error for unsupported layout", func(t *testing.T) {
+		_, err := ParseTime("abc")
+
+		require.EqualError(t, err, "unrecognized time format abc")
+	})
+}
